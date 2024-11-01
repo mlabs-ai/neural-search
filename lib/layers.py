515076ac48889f38e08bbec2a3a68e245f1d51af
Banashree <sbanashri25@gmail.com>
2024-11-01 18:55:38 +0530

    added UnpackGrid

--- lib/layers.py
+++ lib/layers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from math import prod
 
 class ExpandLinear(nn.Module):
     def __init__(self, dim: int, factor: int):
@@ -30,3 +31,28 @@ class Unpack(nn.Module):
 
     def forward(self, x):
         return x.reshape(*x.shape[:-1], x.shape[-1]//self.size, self.size)
+
+class UnpackGrid(nn.Module):
+    def __init__(self, size):
+        super(UnpackGrid, self).__init__()
+        self.size = size
+
+    def forward(self,x):
+
+        return x.reshape(*x.shape[:-3], x.shape[-3]//3, 3,*x.shape[-2:]).movedim(-3,-1)
+
+
+
+class MultiBatchConv2d(nn.Conv2d):
+
+
+    def forward(self, x):
+        original_shape = x.shape
+
+        single_batch_x = x.reshape(prod(original_shape[:-3]) , *original_shape[-3:])
+
+        single_batch_y = super(MultiBatchConv2d, self).forward(single_batch_x)
+
+        multi_batch_y = single_batch_y.reshape(*original_shape[:-3], *single_batch_y.shape[-3:])
+
+        return multi_batch_y
